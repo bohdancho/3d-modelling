cbe3014f41bc9c9a458ab55f47377c48ac08ae3f
Bohdan Chornocondratenko <bogdan.chornocondratenko@gmail.com>
2020-02-14 14:33:53 +0200

    nav

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,8 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
-	new Mmenu("#menu", {
-		"slidingSubmenus": false,
-		"extensions": [
-			"pagedim-black"
-		]
-	});
+let slideout = new Slideout({
+	'panel': document.querySelector('#panel'),
+	'menu': document.querySelector('#menu'),
+	'padding': 350,
+	'tolerance': 70,
 });
+
+document.querySelector('.menu-btn').addEventListener('click', function() {
+	slideout.toggle();
+});
+
+document.querySelector('.menu__link').addEventListener('click', slideClose);
+
+slideout
+	.on('beforeopen', function() {
+		this.panel.classList.add('panel-open');
+	})
+	.on('open', function() {
+		this.panel.addEventListener('click', slideClose);
+	})
+	.on('beforeclose', function() {
+		this.panel.classList.remove('panel-open');
+		this.panel.removeEventListener('click', slideClose);
+	});
+
+function slideClose(eve) {
+	eve.preventDefault();
+	slideout.close();
+}
